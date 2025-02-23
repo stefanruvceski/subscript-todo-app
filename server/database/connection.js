93c7a624f1995e5932aa3ca0c1732a7ea1ccf58c
stefan.ruvceski <stefanruvceski@gmail.com>
2025-02-23 12:56:19 +0100

    changing old file structure for better readability and maintenance

--- server/database/connection.js
+++
@@ -1,5 +0,0 @@
-// Abstraction layer to handle knex configuration per enviornment.
-const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile.js')[environment];
-
-module.exports = require('knex')(config);
