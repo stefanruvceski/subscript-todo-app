6347d8577f59a147b76b1e2ed21ef78ffd012fb7
stefan.ruvceski <stefanruvceski@gmail.com>
2025-02-23 13:13:11 +0100

    unit tests for every layer of API call down to models and integration tests on route level

--- server/controllers/users/user.js
+++ server/controllers/users/user.js
@@ -3,42 +3,37 @@ const userService = require("../../services/users/user");
 class UserController {
   async createUser(req, res) {
     try {
-      const { body } = req;
-      const user = await userService.createUser(body);
+      const user = await userService.createUser(req.body);
       res.status(201).json(user);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
   }
 
   async getUser(req, res) {
     try {
-      const { id } = req.params;
-      const user = await userService.getUserById(id);
-      res.status(200).json(user);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
+      const user = await userService.getUserById(req.params.id);
+      res.json(user);
+    } catch (error) {
+      res.status(404).json({ error: error.message });
     }
   }
 
   async updateUser(req, res) {
     try {
-      const { id } = req.params;
-      const { body } = req;
-      const user = await userService.updateUser(id, body);
-      res.status(200).json(user);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
+      const user = await userService.updateUser(req.params.id, req.body);
+      res.json(user);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
   }
 
   async deleteUser(req, res) {
     try {
-      const { id } = req.params;
-      await userService.deleteUser(id);
-      res.status(204).json();
-    } catch (err) {
-      res.status(400).json({ message: err.message });
+      const user = await userService.deleteUser(req.params.id);
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(404).json({ error: error.message });
     }
   }
 }
